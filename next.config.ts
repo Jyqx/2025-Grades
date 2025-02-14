a9816afcbb80f2878b21d16ef7feba9a2c10f794
Luke Donaldson-Scott <ldo59@uclive.ac.nz>
2025-02-15 12:44:31 +1300

    new

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
-const nextConfig = {
+const nextConfig: NextConfig = {
 	reactStrictMode: true,
 	images: {
 		unoptimized: true, // Disable default image optimization
 	},
 	assetPrefix: isProd ? "/2025-Grades/" : "",
 	basePath: isProd ? "/2025-Grades" : "",
+	trailingSlash: true,
 	output: "export",
 };
 
