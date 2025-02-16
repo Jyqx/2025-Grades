38dc8ff35c9315880d26b65f2bc4619f33d88a23
Luke Donaldson-Scott <ldo59@uclive.ac.nz>
2025-02-16 23:31:24 +1300

    Class assessments added

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,13 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
-const nextConfig: NextConfig = {
+const nextConfig = {
 	reactStrictMode: true,
 	images: {
 		unoptimized: true, // Disable default image optimization
 	},
 	assetPrefix: isProd ? "/2025-Grades/" : "",
 	basePath: isProd ? "/2025-Grades" : "",
-	trailingSlash: true,
 	output: "export",
 };
 
